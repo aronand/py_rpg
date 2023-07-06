52d08f514245841df82864ad89af2dd00aa5b0d1
aronand <71503746+aronand@users.noreply.github.com>
2023-07-06 20:43:32 +0300

    Adds Raylib

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+import pyray
+
+
 def main() -> None:
-    return
+    pyray.init_window(800, 600, "py_rpg")
+    while not pyray.window_should_close():
+        pyray.begin_drawing()
+        pyray.clear_background(pyray.WHITE)
+        pyray.draw_text("py_rpg", 100, 100, 24, pyray.BLACK)
+        pyray.end_drawing()
+    pyray.close_window()
 
 
 if __name__ == "__main__":
