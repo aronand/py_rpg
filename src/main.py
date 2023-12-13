992777db2dbd7a27f3d5713389e9623a93af219c
aronand <71503746+aronand@users.noreply.github.com>
2023-12-13 22:51:25 +0200

    Moves the game loop within Game class

--- src/main.py
+++ src/main.py
@@ -4,14 +4,25 @@ from itemrepository import ItemRepository
 import pyray
 
 
+class Game:
+    __slots__ = ["__name"]
+
+    def __init__(self, name: str):
+        self.__name = name
+        pyray.init_window(800, 600, self.__name)
+
+    def run(self) -> None:
+        while not pyray.window_should_close():
+            pyray.begin_drawing()
+            pyray.clear_background(pyray.WHITE)
+            pyray.draw_text(self.__name, 100, 100, 24, pyray.BLACK)
+            pyray.end_drawing()
+        pyray.close_window()
+
+
 def main() -> None:
-    pyray.init_window(800, 600, "py_rpg")
-    while not pyray.window_should_close():
-        pyray.begin_drawing()
-        pyray.clear_background(pyray.WHITE)
-        pyray.draw_text("py_rpg", 100, 100, 24, pyray.BLACK)
-        pyray.end_drawing()
-    pyray.close_window()
+    game = Game("py_rpg")
+    game.run()
 
 
 if __name__ == "__main__":
