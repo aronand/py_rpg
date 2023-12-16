cda3ec8641ebb4aa42ce4d9a4b83db9cf2a00b2b
aronand <71503746+aronand@users.noreply.github.com>
2023-12-16 18:47:00 +0200

    Implements finding and removing Node's child objects

--- py_rpg/core/node.py
+++ py_rpg/core/node.py
@@ -6,10 +6,21 @@ from attrs import define, field
 
 @define
 class Node:
-    parent: Self | None = field(init=False, default=None)
+    name: str = field(default="Node")
+    parent: Self | None = field(default=None)
     child_nodes: list[Self] = field(init=False, factory=list)
 
     def add_child(self, node: Self) -> None:
         if not issubclass(type(node), Node):
             raise TypeError
         self.child_nodes.append(node)
+
+    def remove_child(self, idx: int) -> Self:
+        return self.child_nodes.pop(idx)
+
+    def find_child(self, name: str) -> Self | None:
+        for child in self.child_nodes:
+            if child.name != name:
+                continue
+            return child
+        return None
--- py_rpg/tests/test_node.py
+++ py_rpg/tests/test_node.py
@@ -21,7 +21,14 @@ class TestNode(TestCase):
         self.assertEqual(1, len(self.node.child_nodes))
 
     def test_removing_child_nodes(self) -> None:
-        self.fail()
+        self.node.add_child(Node())
+        self.assertEqual(1, len(self.node.child_nodes))
+        removed = self.node.remove_child(0)
+        self.assertTrue(issubclass(type(removed), Node))
+        self.assertEqual(0, len(self.node.child_nodes))
 
     def test_finding_child_nodes(self) -> None:
-        self.fail()
+        child_name = "TestChild"
+        self.node.add_child(Node(child_name))
+        self.assertEqual(child_name, self.node.find_child(child_name).name)
+        self.assertIsNone(self.node.find_child("DoesNotExist"))
