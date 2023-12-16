201ccec073b4649445fb7159b8075d08dadf15c2
aronand <71503746+aronand@users.noreply.github.com>
2023-12-16 18:23:08 +0200

    Adds tests to core.Node and implements adding child nodes.

--- py_rpg/core/node.py
+++ py_rpg/core/node.py
@@ -1,6 +1,15 @@
-from attrs import define
+"""A base class for game objects. Heavily inspired by Godot."""
+from typing import Self
+
+from attrs import define, field
 
 
 @define
 class Node:
-    pass
+    parent: Self | None = field(init=False, default=None)
+    child_nodes: list[Self] = field(init=False, factory=list)
+
+    def add_child(self, node: Self) -> None:
+        if not issubclass(type(node), Node):
+            raise TypeError
+        self.child_nodes.append(node)
--- py_rpg/tests/test_node.py
+++ py_rpg/tests/test_node.py
@@ -4,4 +4,24 @@ from py_rpg.core import Node
 
 
 class TestNode(TestCase):
-    pass
+    def setUp(self) -> None:
+        self.node = Node()
+
+    def test_initial_parent_node(self) -> None:
+        self.assertIsNone(self.node.parent)
+
+    def test_initial_child_nodes(self) -> None:
+        self.assertEqual(0, len(self.node.child_nodes))
+
+    def test_adding_child_nodes(self) -> None:
+        with self.assertRaises(TypeError):
+            self.node.add_child(123)
+
+        self.node.add_child(Node())
+        self.assertEqual(1, len(self.node.child_nodes))
+
+    def test_removing_child_nodes(self) -> None:
+        self.fail()
+
+    def test_finding_child_nodes(self) -> None:
+        self.fail()
