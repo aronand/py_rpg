cc56a3e73518ef7a11c6b560f10ce2f92162e040
aronand <71503746+aronand@users.noreply.github.com>
2023-12-17 15:46:52 +0200

    Adds levels and stamina for characters

--- py_rpg/character.py
+++ py_rpg/character.py
@@ -4,16 +4,51 @@ import pyray
 
 
 class Character(KinematicBody):
-    __slots__ = ["__name", "texture"]
+    __slots__ = ["__name", "texture", "__level", "__max_stamina", "__cur_stamina"]
 
     def __init__(self, name: str, position: pyray.Vector2 = pyray.Vector2(0, 0)):
         self.__name = name
+        self.__level: int = 1
+        self.__max_stamina: int = 100
+        self.__cur_stamina: int = self.__max_stamina
         self.texture: pyray.Texture2D | None = None
         super().__init__(position)
 
     @property
     def character_name(self) -> str:  # can't use name, as this clashes with Node.name
         return self.__name
+
+    @property
+    def level(self) -> int:
+        return self.__level
+
+    @property
+    def max_stamina(self) -> int:
+        return self.__max_stamina
+
+    @property
+    def cur_stamina(self) -> int:
+        return self.__cur_stamina
+
+    @cur_stamina.setter
+    def cur_stamina(self, value: int) -> None:
+        if value > self.__max_stamina:
+            # TODO: Possibly raise an exception?
+            return
+        if type(value) is not int:
+            # TODO: Possibly raise an exception?
+            return
+        if value < 0:
+            value = 0
+        self.__cur_stamina = value
+
+    @property
+    def is_dead(self) -> bool:
+        return self.__cur_stamina <= 0
+
+    def level_up(self) -> None:
+        self.__level += 1
+        self.__max_stamina = int(1.1 * self.__max_stamina)
 
     def update(self) -> None:
         # We need to call the KinematicBody's update as we are inheriting it
--- py_rpg/tests/test_character.py
+++ py_rpg/tests/test_character.py
@@ -34,3 +34,28 @@ class TestCharacter(TestCase):
         with self.subTest("Test Character.next_x and Character.next_y"):
             self.assertEqual(32.0, self.character1.next_x)
             self.assertEqual(64.0, self.character1.next_y)
+
+    def test_stamina_properties(self) -> None:
+        max_stamina = self.character1.max_stamina
+        self.assertEqual(max_stamina, self.character1.cur_stamina)
+        # Test that cur_stamina can't be made higher than max_stamina
+        self.character1.cur_stamina = max_stamina + 1
+        self.assertEqual(max_stamina, self.character1.cur_stamina)
+        # Test that other value types, such as float, are not supported
+        self.character1.cur_stamina = 80.0
+        self.assertEqual(max_stamina, self.character1.cur_stamina)
+        # Test that negative values become zero
+        self.character1.cur_stamina = -1000
+        self.assertEqual(0, self.character1.cur_stamina)
+
+    def test_is_dead(self) -> None:
+        self.assertFalse(self.character1.is_dead)
+        self.character1.cur_stamina = 0
+        self.assertTrue(self.character1.is_dead)
+
+    def test_level_up(self) -> None:
+        max_stamina = self.character1.max_stamina
+        self.assertEqual(1, self.character1.level)
+        self.character1.level_up()
+        self.assertEqual(2, self.character1.level)
+        self.assertGreater(self.character1.max_stamina, max_stamina)
