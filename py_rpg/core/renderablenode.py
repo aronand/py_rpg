ef32b6ca48b55fdf7d7d53fbeb3f6eb37946db25
aronand <71503746+aronand@users.noreply.github.com>
2023-12-19 01:31:41 +0200

    Adds visible flag to RenderableNode

--- py_rpg/core/renderablenode.py
+++ py_rpg/core/renderablenode.py
@@ -4,10 +4,11 @@ import pyray
 
 
 class RenderableNode(Node):
-    __slots__ = ("position")
+    __slots__ = "position", "visible"
 
     def __init__(self) -> None:
         self.position = pyray.Vector2(0, 0)
+        self.visible: bool = True
         super().__init__()
 
     def render(self) -> None:
