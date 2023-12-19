d9579d7af811d883d6c539e80a4551b7dcfe589f
aronand <71503746+aronand@users.noreply.github.com>
2023-12-19 12:30:06 +0200

    Adds raywrap.mode_2d context manager
    Objects rendered within mode_2d will be drawn in the world space.

--- py_rpg/raywrap.py
+++ py_rpg/raywrap.py
@@ -6,12 +6,27 @@ import pyray
 
 class drawing:
     """Context manager for Raylib drawing."""
+    def __enter__(self) -> None:
+        pyray.begin_drawing()
+        pyray.clear_background(pyray.WHITE)
+
+    def __exit__(self, 
+                 type: Optional[Type[BaseException]],
+                 value: Optional[BaseException],
+                 traceback: Optional[TracebackType]
+    ) -> Literal[False]:
+        pyray.end_drawing()
+        return False  # return True to suppress raised exceptions
+
+class mode_2d:
+    """Context manager for Raylib mode_2d.
+    
+    Objects rendered within here will be drawn in the world space.
+    """
     def __init__(self, camera: pyray.Camera2D) -> None:
         self.__camera = camera
 
     def __enter__(self) -> None:
-        pyray.begin_drawing()
-        pyray.clear_background(pyray.WHITE)
         pyray.begin_mode_2d(self.__camera)
 
     def __exit__(self, 
@@ -20,5 +35,4 @@ class drawing:
                  traceback: Optional[TracebackType]
     ) -> Literal[False]:
         pyray.end_mode_2d()
-        pyray.end_drawing()
         return False  # return True to suppress raised exceptions
