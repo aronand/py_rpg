4d89bc50aec0c5fceccaec1ee5513af1bc227858
aronand <71503746+aronand@users.noreply.github.com>
2023-12-25 18:43:09 +0200

    refactor: Node class changes to child nodes and parenting
    Child nodes are now stored in dict instead of a list.
    
    A Node no longer can be parented via parent.setter. Instead, parent can only be altered via Node.add_child() and Node.remove_child. Node.add_child() no longer accepts nodes that already have a parent.

--- py_rpg/core/node.py
+++ py_rpg/core/node.py
@@ -10,51 +10,48 @@ class Node:
                  parent: Optional["Node"] = None) -> None:
         self.name = node_name
         self.__parent: "Node" | None = None
-        self.parent = parent
-        self.child_nodes: list["Node"] = []
+        if parent:
+            parent.add_child(self)
+        self.child_nodes: dict[str, "Node"] = {}
 
     @property
     def parent(self) -> Optional["Node"]:
         return self.__parent
 
-    @parent.setter
-    def parent(self, value: Optional["Node"]) -> None:
-        if self.__parent is not None:
-            self.__parent.child_nodes.remove(self)
-        self.__parent = value
-        if value is None:
-            return
-        value.add_child(self)
-
     def add_child(self, node: "Node") -> None:
         """Adds a child object to the Node.
         
         :raises TypeError: Argument is not a subclass of Node
+        :raises RuntimeError: Node already has a parent
         """
         if not issubclass(type(node), Node):
             raise TypeError
-        self.child_nodes.append(node)
+        if node.parent is not None:
+            raise RuntimeError
+        node_name: str = node.name
+        self.child_nodes[node_name] = node
         node.__parent = self
 
-    def remove_child(self, idx: int) -> "Node":
-        """Removes and returns a child object at given index.
+    def remove_child(self, node_name: str) -> Optional["Node"]:
+        """Removes and returns a child object with a given name, or None if not found.
+        Sets child node's parent to None.
         
-        :returns: Node
-        :raises IndexError: Given index is invalid
+        :returns: Node | None
         """
-        return self.child_nodes.pop(idx)
+        node = self.child_nodes.pop(node_name, None)
+        if node:
+            node.__parent = None
+        return node
 
-    def find_child(self, name: str) -> Optional["Node"]:
+    def find_child(self, node_name: str) -> Optional["Node"]:
         """Finds and returns a child object with a given name.
         
         Only returns the first object with a matching name, or None if no matches found.
 
-        :returns: Node or None
+        :returns: Node | None
         """
-        for child in self.child_nodes:
-            if child.name != name:
-                continue
-            return child
+        if node_name in self.child_nodes:
+            return self.child_nodes[node_name]
         return None
 
     def update(self) -> None:
