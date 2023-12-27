dac6a058f84ceba922353d788f16d22e8a742e6d
aronand <71503746+aronand@users.noreply.github.com>
2023-12-27 10:28:38 +0200

    feat: size and title properties for Window

--- py_rpg/window.py
+++ py_rpg/window.py
@@ -2,10 +2,11 @@ import pyray
 
 
 class Window:
-    __slots__ = ()
+    __slots__ = ("__title")
 
     def __init__(self, width: int, height: int, title: str) -> None:
-        pyray.init_window(width, height, title)
+        self.__title = title
+        pyray.init_window(width, height, self.__title)
 
     def __del__(self) -> None:
         pyray.close_window()
@@ -17,3 +18,20 @@ class Window:
     @property
     def height(self) -> int:
         return int(pyray.get_screen_height())
+
+    @property
+    def size(self) -> tuple[int, int]:
+        return self.width, self.height
+
+    @size.setter
+    def size(self, size: tuple[int, int]) -> None:
+        pyray.set_window_size(size[0], size[1])
+
+    @property
+    def title(self) -> str:
+        return self.__title
+
+    @title.setter
+    def title(self, new_title: str) -> None:
+        self.__title = new_title
+        pyray.set_window_title(new_title)
